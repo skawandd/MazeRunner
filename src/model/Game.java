247c8275584bbdadf1e0d4d8f6ed60f8cc6c48c4
skawandd <dylan.skawand@gmail.com>
2019-05-09 11:42:20 +0200

    testMap1

--- src/model/Game.java
+++ src/model/Game.java
@@ -8,14 +8,20 @@ public class Game {
 	private Square[][] board;
 	
 	public Game() {
-		this.board = new Square[10][10];
+		this.board = testMap();
 	}
 	
 	public int start() {
-		this.board[0][0] = new Floor();
+		/*
+		board[0][0] = new Floor();
 		this.board[0][0] = new Human(board[0][0]);
 		System.out.println((board[0][0].isAlive()));
+		*/
 		System.out.println(new Controller().getKeyboard());
 		return 0;
+	}
+	
+	public Square[][] testMap() {
+		return null;
 	}
 }
